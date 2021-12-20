12bddcce366e78f40689eafd51a64a156ea73922
lunaStratos <lunastratos@gmail.com>
2021-12-20 23:05:06 +0900

    Mail Sender
    Java Mail Sender Up

---
+++ mudspoon/src/main/kotlin/com/lunastratos/mudspoon/Util/Mail/MailSender.kt
@@ -0,0 +1,38 @@
+package com.lunastratos.mudspoon.Util.Mail
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.mail.javamail.MimeMessageHelper
+import org.springframework.stereotype.Service
+
+/**
+ * 인증메일 보내기
+ * @version     1.0
+ * @author      LunaStratos
+ * */
+@Service
+class MailSender {
+
+    @Autowired
+    lateinit var javaMailSender: JavaMailSender
+
+    fun mailSend(email: String, token: String) {
+        val mimeMessage = javaMailSender.createMimeMessage()
+        val mimeMessageHelper = MimeMessageHelper(mimeMessage)
+
+        mimeMessageHelper.setSubject("[흙수저프랜즈] 인증메일 입니다.")
+        mimeMessageHelper.setTo(email)
+
+        val html:String = "<html> <body> <h3>인증메일 입니다.<h3> " +
+                "<br>아래의 링크를 눌러서 인증해 주세요." +
+                "<br> 인증번호: ${token} " +
+                "<br> <a href='http://local.xrland.net/api/demo/account/auth-check-action?&email=${email}&token=${token}'>다이렉트 인증하기</a>" +
+                "<br> 위 링크가 안된다면 아래의 창에서 인증하여 주십시오." +
+                "<br> <a href='http://local.xrland.net/register?email=${email}'>인증페이지</a>" +
+                "</body></html>"
+        mimeMessageHelper.setText(html, true )
+
+        javaMailSender.send(mimeMessage)
+    }
+
+}
